aaff92a7fe15a5b0c04153d1ee816d34f90bf5d2
theehmedov <the.ehmedov008@gmail.com>
2025-12-06 19:28:49 +0400

    Update api.py

--- task3/back-end/api.py
+++ task3/back-end/api.py
@@ -1,12 +1,14 @@
 from flask import Flask
-from flask_cors import CORS
+from flask_cors import CORS # CORS dəstəyini əlavə edin
 
 app = Flask(__name__)
-CORS(app)
+CORS(app) # CORS-u tətbiqdə aktivləşdirin
 
-@app.route('/api/hello')
+@app.route('/api/hello') # Marşrutu /api/hello olaraq dəyişin
 def hello_world():
+    # Sadə string cavabı
     return 'Hello, World!'
 
 if __name__ == '__main__':
+    # Portu 5252 olaraq dəyişin
     app.run(host='0.0.0.0', port=5252)
